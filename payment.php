e841d8b19f4b246bd805077bf73fd2ae24c5e33d
Daniel James Freburg <djfrebu@ilstu.edu>
2020-04-24 15:36:56 -0500

    updates

--- payment.php
+++ payment.php
@@ -1,34 +1,27 @@
-<!-- Checkout webpage html -->
+<!-- Checkout webpage -->
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ISU Textbook Market</title>
-
     <link href="bootstrap-3.0.0/dist/css/bootstrap.css" rel="stylesheet">
-
     <link href="IT353group.css" rel="stylesheet">
-
   </head>
   <body>
 <?php
 	include('header.php')
 ?>
-
 		<div class="container">
 			<div class="row">
 				<div class="col-md-3">
 					<img style="width: 100%;" src="images/shot pict/redbirdISU.jpg" alt="redbird">
 				</div>
-
 <h1>PAYMENT PROCESSED</h1>
 <h2>Thanks for using ISU Textbook Market</h2>
-
 </div>
 </div>
 </form>
-  
 <?php
 	include('footer.php')
 ?>
